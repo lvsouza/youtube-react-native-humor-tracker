0ae9e69afa91f9f9cc9047e0e0b8006178f2981e
Lucas Souza <ls18017@gmail.com>
2025-07-11 15:39:52 -0300

    feat: conditional rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,26 @@
 import { useState } from 'react';
-import { Text } from 'react-native';
+import { Text, TouchableOpacity } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
-import { MyButton } from './shared/components/MyButton';
 
 
 export default function App() {
-  const [state, setState] = useState('Teste');
+  const [hide, setHide] = useState(false);
 
   console.log('render')
 
 
   return (
     <SafeAreaView>
-      <MyButton order={1} onPress={() => setState('Teste 2')}>
-        <Text>MyButton {state}</Text>
-      </MyButton>
+      {hide
+        ? <Text>Showed</Text>
+        : <Text>Hided</Text>
+      }
 
+      <TouchableOpacity onPress={() => setHide(!hide)}>
+        <Text>Toggle</Text>
+      </TouchableOpacity>
 
-      <MyButton>
-        <Text>MyButton</Text>
-      </MyButton>
-
-      <MyButton onPress={() => console.log('teste')} />
     </SafeAreaView>
   );
 }
