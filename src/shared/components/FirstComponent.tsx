fe5a45eed83422680355511df41301018859a8e4
Lucas Souza <ls18017@gmail.com>
2025-07-11 09:59:49 -0300

    feat: add first component

---
+++ src/shared/components/FirstComponent.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from 'react-native';
+
+
+interface IFirstComponentProps {
+  order: number;
+}
+export const FirstComponent = (props: IFirstComponentProps) => {
+
+  return (
+    <View>
+      <Text>FirstComponent {props.order}</Text>
+    </View>
+  );
+}
